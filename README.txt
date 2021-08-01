f11a4455098e9dfa6db0b407457a6450d94b34ed
Deepesh18 <88265238+Deepesh18@users.noreply.github.com>
2021-08-01 12:10:27 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+Login and Registration page having dummy datas with HTML validations
+ Sample data file:Json.txt
+ Available fields:Name,Age,E-mail Id,Phone No,Password
+ Display page will display all the sample records
+ On select name , particular rec page will show the particular records
---
+++ json.php
@@ -0,0 +1,32 @@
+[
+  {
+    "id": "1",
+    "name": "Abi",
+    "phoneno": "9877897898",
+    "age": "21",
+    "emailId": "test@gmail.com",
+	"pass": "123"
+ },
+   {
+    "id": "2",
+    "name": "ram",
+    "phoneno": "9877897888",
+    "age": "21",
+    "emailId": "test1@gmail.com",
+	"pass": "123"
+ },  {
+    "id": "3",
+    "name": "hari",
+    "phoneno": "9877897878",
+    "age": "21",
+    "emailId": "test2@gmail.com",
+	"pass": "123"
+ },  {
+    "id": "4",
+    "name": "sam",
+    "phoneno": "9877897818",
+    "age": "21",
+    "emailId": "test3@gmail.com",
+	"pass": "123"
+ }
+]
